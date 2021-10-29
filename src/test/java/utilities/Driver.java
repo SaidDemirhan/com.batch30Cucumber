abeea02c4baf7b975c4f853b3b2750f1f89ae40e
said <saiddemirhan@gmail.com>
2021-10-29 20:36:57 +0200

    cucumber

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,71 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    private static WebDriver driver;
+
+    private Driver(){
+
+    }
+    //driver i baska class lardan cagirabilmek icin static yaptik
+    //henuz driverlarl  ilgili atama ve ayar yapilmadigi icin
+    //yanlis kullanimlarin önune gecmek icin privat yaptik.
+
+    public static WebDriver getDriver() {
+
+
+
+        if (driver == null) {
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    break;
+
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        }
+            return driver;
+        }
+
+        public static void closeDriver () {
+
+        if (driver!=null) {
+            driver.quit();
+
+        }
+
+        driver=null;
+        }
+
+
+
+}
